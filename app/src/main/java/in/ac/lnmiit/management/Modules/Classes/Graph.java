f22c319a631dee1026b20b3bb3268cb5c77d4817
iamvs-2002 <iamvs2002@gmail.com>
2022-02-23 14:38:18 +0530

    Implemented Clique Partitioning Algorithm for Time Table Generation Module

---
+++ app/src/main/java/in/ac/lnmiit/management/Modules/Classes/Graph.java
@@ -0,0 +1,99 @@
+package in.ac.lnmiit.management.Modules.Classes;
+
+import android.util.Log;
+
+import java.util.*;
+
+public class Graph {
+    public static boolean[] visited;
+    static Map<Integer, Integer> V = new HashMap<>();
+    // TreeSet is used to get clear understand of graph.
+    static HashMap<Integer, TreeSet<Integer>> graph;
+
+    // Graph Constructor
+    public Graph(int v) {
+        graph = new HashMap<>();
+        for (int i = 1; i <= v; i++) {
+            graph.put(i, new TreeSet<>());
+        }
+    }
+    // Adds an edge to an undirected graph
+    public void addEdge(int src, int dest) {
+        // Add an edge from src to dest into the set
+        graph.get(src).add(dest);
+        // Since graph is undirected, add an edge
+        // from dest to src into the set
+        graph.get(dest).add(src);
+    }
+    public static int Findclique(int no_of_subject) {
+        Log.e("TAG","Inside find clique");
+        int v = no_of_subject;
+        visited = new boolean[v + 1];
+        int subject = 1;
+        int count = 0;
+        for (int i = 1; i <= no_of_subject; i++) {
+            if (graph.containsKey(i))
+                V.put(i, graph.get(i).size());
+        }
+        while (subject <= no_of_subject) {
+            if (visited[subject]) {
+                subject++;
+            } else {
+                Stack<Integer> clique = new Stack<Integer>();
+                makeclique(subject, clique);
+                count++;
+                subject++;
+                System.out.println("clique " + count + ":");
+                while (!clique.isEmpty()) {
+                    System.out.print(clique.pop() + " ");
+                }
+
+                System.out.println(" ");
+                for (int i = 1; i <= no_of_subject; i++) {
+                    if (graph.containsKey(i))
+                        V.put(i, graph.get(i).size());
+                }
+            }
+        }
+        return count;
+    }
+
+    public static void makeclique(int subject, Stack<Integer> clique) {
+        Log.e("TAG","Inside make clique");
+        clique.add(subject);
+        visited[subject] = true;
+        Map<Integer, Integer> adjacent = FindAdjacent(subject);
+
+        Map<Integer, Integer> newV = new HashMap<>();
+
+        for (Map.Entry<Integer, Integer> entry : adjacent.entrySet()) {
+            int x = entry.getKey();
+            if (V.containsKey(x)) {
+                newV.put(x, entry.getValue());
+            }
+        }
+        V = newV;
+        if (!V.isEmpty()) {
+            int max_degree_node = Integer.MIN_VALUE;
+            for (Map.Entry<Integer, Integer> entry : V.entrySet()) {
+                if (entry.getValue() > max_degree_node) {
+                    max_degree_node = entry.getKey();
+                }
+            }
+            makeclique(max_degree_node, clique);
+        }
+
+    }
+
+    public static Map<Integer, Integer> FindAdjacent(int subject) {
+        Map<Integer, Integer> adjacent = new HashMap<>();
+        if (graph.containsKey(subject)) {
+            Iterator<Integer> set = graph.get(subject).iterator();
+            while (set.hasNext()) {
+                int x = set.next();
+                adjacent.put(x, graph.get(x).size());
+            }
+        }
+        return adjacent;
+    }
+}
--- app/src/main/java/in/ac/lnmiit/management/Modules/TimeTableGeneration.java
+++ app/src/main/java/in/ac/lnmiit/management/Modules/TimeTableGeneration.java
@@ -4,9 +4,23 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.AppCompatButton;
 import androidx.appcompat.widget.Toolbar;
+import androidx.documentfile.provider.DocumentFile;
+import androidx.loader.content.CursorLoader;
 
+import android.app.Activity;
+import android.content.ContentUris;
+import android.content.Context;
 import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
+import android.os.FileUtils;
+import android.provider.DocumentsContract;
+import android.provider.MediaStore;
+import android.provider.OpenableColumns;
+import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ImageButton;
@@ -15,6 +29,26 @@ import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import org.apache.poi.hssf.usermodel.HSSFSheet;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.poifs.filesystem.POIFSFileSystem;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.URI;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeSet;
+import java.util.regex.Pattern;
+
+import in.ac.lnmiit.management.Modules.Classes.Graph;
 import in.ac.lnmiit.management.R;
 
 public class TimeTableGeneration extends AppCompatActivity {
@@ -28,6 +62,7 @@ public class TimeTableGeneration extends AppCompatActivity {
     TextView excelFileName;
     static boolean isFileSelected;
     ProgressBar timetableProgressBar;
+    static File file;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -47,13 +82,13 @@ public class TimeTableGeneration extends AppCompatActivity {
         timetableProgressBar = findViewById(R.id.timetableProgressBar);
 
         //Creating the ArrayAdapter instance having the semesters name list
-        ArrayAdapter semestersAdapter = new ArrayAdapter(this,android.R.layout.simple_spinner_item,semesters);
+        ArrayAdapter semestersAdapter = new ArrayAdapter(this, android.R.layout.simple_spinner_item, semesters);
         semestersAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         //Setting the ArrayAdapter data on the Spinner
         semestersSpinner.setAdapter(semestersAdapter);
 
         //Creating the ArrayAdapter instance having the program name list
-        ArrayAdapter programsAdapter = new ArrayAdapter(this,android.R.layout.simple_spinner_item,programs);
+        ArrayAdapter programsAdapter = new ArrayAdapter(this, android.R.layout.simple_spinner_item, programs);
         programsAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         //Setting the ArrayAdapter data on the Spinner
         programsSpinner.setAdapter(programsAdapter);
@@ -70,22 +105,23 @@ public class TimeTableGeneration extends AppCompatActivity {
         generateTimeTablebtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if(semestersSpinner.getSelectedItem() == null){
+                if (semestersSpinner.getSelectedItem() == null) {
                     Toast.makeText(TimeTableGeneration.this, "Kindly select a semester.", Toast.LENGTH_SHORT).show();
                     return;
                 }
-                if(programsSpinner.getSelectedItem() == null){
+                if (programsSpinner.getSelectedItem() == null) {
                     Toast.makeText(TimeTableGeneration.this, "Kindly select a program.", Toast.LENGTH_SHORT).show();
                     return;
                 }
-                if(!isFileSelected){
+                if (!isFileSelected) {
                     Toast.makeText(TimeTableGeneration.this, "Kindly select the excel file.", Toast.LENGTH_SHORT).show();
                     return;
                 }
                 timetableProgressBar.setVisibility(View.VISIBLE);
                 String sem = semestersSpinner.getSelectedItem().toString();
                 String prog = programsSpinner.getSelectedItem().toString();
-                Toast.makeText(TimeTableGeneration.this, "Generating time table for "+prog+" "+sem+".", Toast.LENGTH_SHORT).show();
+                findCliques(file);
+                Toast.makeText(TimeTableGeneration.this, "Generating time table for " + prog + " " + sem + ".", Toast.LENGTH_SHORT).show();
                 timetableProgressBar.setVisibility(View.GONE);
             }
         });
@@ -97,12 +133,12 @@ public class TimeTableGeneration extends AppCompatActivity {
                 Toast.makeText(TimeTableGeneration.this, "History button clicked", Toast.LENGTH_SHORT).show();
             }
         });
-
     }
+
     private void selectfile() {
         // select the file from the file storage
         Intent intent = new Intent(Intent.ACTION_OPEN_DOCUMENT);
-        intent.setType("*/*");
+        intent.setType("application/*");
         intent.addCategory(Intent.CATEGORY_OPENABLE);
         startActivityForResult(Intent.createChooser(intent, "Select timetable file..."), 102);
     }
@@ -110,13 +146,22 @@ public class TimeTableGeneration extends AppCompatActivity {
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
         if (requestCode == 102) {
+            Log.e("TAG", "Req code verified");
             if (resultCode == RESULT_OK) {
-                String filepath = data.getData().getPath();
+
+                Uri uri = data.getData();
+                String filename = getFileName(uri);
+
+                File myFile = new File(uri.getPath());
+
+                Log.e("TAG", "Path: " + uri.getPath());
+                Log.e("TAG", "Exists: " + myFile.exists());
+
                 // If excel file then only select the file
-                if (filepath.endsWith(".xlsx") || filepath.endsWith(".xls")) {
+                if (filename.endsWith(".xlsx") || filename.endsWith(".xls")) {
                     isFileSelected = true;
-                    excelFileName.setText(filepath);
-                    Toast.makeText(this, "File selected successfully.", Toast.LENGTH_SHORT).show();
+                    excelFileName.setText(filename);
+                    file = myFile;
                 }
                 // else show the error
                 else {
@@ -124,5 +169,56 @@ public class TimeTableGeneration extends AppCompatActivity {
                 }
             }
         }
+    }
+
+    private String getFileName(Uri uri) throws IllegalArgumentException {
+        // Obtain a cursor with information regarding this uri
+        Cursor cursor = getContentResolver().query(uri, null, null, null, null);
+
+        if (cursor.getCount() <= 0) {
+            cursor.close();
+            throw new IllegalArgumentException("Can't obtain file name, cursor is empty");
+        }
+
+        cursor.moveToFirst();
+
+        String fileName = cursor.getString(cursor.getColumnIndexOrThrow(OpenableColumns.DISPLAY_NAME));
+
+        cursor.close();
+
+        return fileName;
+    }
+    void findCliques(File file) {
+        timetableProgressBar.setVisibility(View.VISIBLE);
+        Map<String, TreeSet<String>> courses = new HashMap<>();
+        // Create the graph given in the above figure
+        try {
+            Log.e("TAG", "Finding Cliques");
+            Log.e("TAG", file.exists() + "");
+            FileInputStream fis = new FileInputStream(file);   //obtaining bytes from the file
+            Log.e("TAG", "input stream");
+
+            // Create a POIFSFileSystem object
+            POIFSFileSystem myFileSystem = new POIFSFileSystem(fis);
+            Log.e("TAG", "File system");
+
+            // Creating Workbook instance that refers to .xlsx file
+            HSSFWorkbook wb = new HSSFWorkbook(myFileSystem);
+            HSSFSheet sheet = wb.getSheetAt(0);     //creating a Sheet object to retrieve object
+
+            Log.e("TAG", "workbook sheet");
+            int row = sheet.getLastRowNum();
+            int col = sheet.getRow(1).getLastCellNum();
+
+            int v = col; // number of subjects
+            Graph graph = new Graph(v);
+            Log.e("TAG", "Finding Cliques 2");
+            Toast.makeText(TimeTableGeneration.this, "Total number of clique: " + Graph.Findclique(v), Toast.LENGTH_SHORT).show();
+            // System.out.print("Total number of clique: " + Graph.Findclique(v));
+        } catch (Exception e) {
+            Log.e("TAG", "Some error while reading excel file" + e);
+            e.printStackTrace();
+        }
+        timetableProgressBar.setVisibility(View.GONE);
     }
 }
