d134fbb3caac454e1eb2e9561ecb8d517601684f
iamvs-2002 <iamvs2002@gmail.com>
2022-02-24 15:42:49 +0530

    Dispensary Management UI - Student/Doctor options

--- app/src/main/java/in/ac/lnmiit/management/Modules/DispensaryManagement.java
+++ app/src/main/java/in/ac/lnmiit/management/Modules/DispensaryManagement.java
@@ -4,12 +4,17 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.LinearLayout;
 
 import in.ac.lnmiit.management.R;
 
 public class DispensaryManagement extends AppCompatActivity {
 
     private Toolbar toolbar;
+    LinearLayout studentLayout, doctorLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,5 +24,30 @@ public class DispensaryManagement extends AppCompatActivity {
         toolbar = findViewById(R.id.toolbar);
         toolbar.setTitle("Dispensary Management");
         setSupportActionBar(toolbar);
+
+        studentLayout = findViewById(R.id.student_dispensary_layout);
+        doctorLayout = findViewById(R.id.doctor_dispensary_layout);
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.dispensarymenu, menu);
+        return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle item selection
+        switch (item.getItemId()) {
+            case R.id.dispensary_doctor_login_menuoption:
+                doctorLayout.setVisibility(View.VISIBLE);
+                studentLayout.setVisibility(View.GONE);
+                return true;
+            case R.id.dispensary_student_login_menuoption:
+                doctorLayout.setVisibility(View.GONE);
+                studentLayout.setVisibility(View.VISIBLE);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
